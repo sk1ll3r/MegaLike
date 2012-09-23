216c46808c0fc307310d58cbf97eed3dc5eb1d9d
Tuan Anh Le <skillerr@gmail.com>
2012-09-23 23:36:29 +0200

    First try
    trying to copy the sample

--- MegaLike/src/com/atlast/MegaLike/MainGalleryActivity.java
+++ MegaLike/src/com/atlast/MegaLike/MainGalleryActivity.java
@@ -1,5 +1,7 @@
 package com.atlast.MegaLike;
 
+import com.viewpagerindicator.TabPageIndicator;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -8,46 +10,40 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 
 public class MainGalleryActivity extends FragmentActivity {
-	private static final int NUMBER_OF_PAGES = 4;
-
-	private ViewPager mViewPager;
-	private MyFragmentPagerAdapter mMyFragmentPagerAdapter;
+	private static final String[] CONTENT = new String[] { "Tagged",
+			"Uploaded", "Starred", "Statuses" };
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_gallery);
-		mViewPager = (ViewPager) findViewById(R.id.viewpager);
-		mMyFragmentPagerAdapter = new MyFragmentPagerAdapter(
-				getSupportFragmentManager());
-		mViewPager.setAdapter(mMyFragmentPagerAdapter);
+
+		FragmentPagerAdapter adapter = new MegalikeAdapter(getSupportFragmentManager());
+
+		ViewPager pager = (ViewPager) findViewById(R.id.pager);
+		pager.setAdapter(adapter);
+
+		TabPageIndicator indicator = (TabPageIndicator) findViewById(R.id.indicator);
+		indicator.setViewPager(pager);
 	}
 
-	private static class MyFragmentPagerAdapter extends FragmentPagerAdapter {
-
-		public MyFragmentPagerAdapter(FragmentManager fm) {
+	private static class MegalikeAdapter extends FragmentPagerAdapter {
+		public MegalikeAdapter(FragmentManager fm) {
 			super(fm);
 		}
 
 		@Override
-		public Fragment getItem(int index) {
-			switch (index) {
-			case 0:
-				return PageFragment.newInstance("Tagged");
-			case 1:
-				return PageFragment.newInstance("Uploaded");
-			case 2:
-				return PageFragment.newInstance("Starred");
-			case 3:
-				return PageFragment.newInstance("Statuses");
-			default:
-				return PageFragment.newInstance("Unkown");
-			}
+		public Fragment getItem(int position) {
+			return PageFragment.newInstance(CONTENT[position % CONTENT.length]);
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return CONTENT[position % CONTENT.length].toUpperCase();
 		}
 
 		@Override
 		public int getCount() {
-
-			return NUMBER_OF_PAGES;
+			return CONTENT.length;
 		}
 	}
 }
