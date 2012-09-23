e1b28073adf144e1b7cb93bcf0af576aba8e0447
Tuan Anh Le <skillerr@gmail.com>
2012-09-24 01:45:29 +0200

    Added collapsible search action item
    ...and action items handling

--- MegaLike/src/com/atlast/MegaLike/MainGalleryActivity.java
+++ MegaLike/src/com/atlast/MegaLike/MainGalleryActivity.java
@@ -1,6 +1,8 @@
 package com.atlast.MegaLike;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 import com.viewpagerindicator.TabPageIndicator;
 
 import android.os.Bundle;
@@ -8,10 +10,25 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.widget.Toast;
 
 public class MainGalleryActivity extends SherlockFragmentActivity {
-	private static final String[] CONTENT = new String[] { "Tagged",
-			"Uploaded", "Starred", "Statuses" };
+	private static final String[] CONTENT = new String[] { "Tagged", "Uploaded", "Starred", "Statuses" };
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add("Search")
+			.setActionView(R.layout.collapsible_edittext)
+			.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS | MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
+		menu.add("Login with different user").setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Toast.makeText(MainGalleryActivity.this, "Got click: " + item, Toast.LENGTH_SHORT).show();
+        return true;
+	}
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
