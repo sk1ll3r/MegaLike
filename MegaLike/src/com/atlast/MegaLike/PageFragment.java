812099c28ca8c9c24fa338aae344e1bfe4b895ed
Tuan Anh Le <skillerr@gmail.com>
2012-09-24 00:13:16 +0200

    Managed to get the tabs + viewpager working

--- MegaLike/src/com/atlast/MegaLike/PageFragment.java
+++ MegaLike/src/com/atlast/MegaLike/PageFragment.java
@@ -2,34 +2,60 @@ package com.atlast.MegaLike;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
 
-public class PageFragment extends Fragment {
+public final class PageFragment extends Fragment {
+    private static final String KEY_CONTENT = "TestFragment:Content";
 
-	public static PageFragment newInstance(String title) {
+    public static PageFragment newInstance(String content) {
+    	PageFragment fragment = new PageFragment();
 
-		PageFragment pageFragment = new PageFragment();
-		Bundle bundle = new Bundle();
-		bundle.putString("title", title);
-		pageFragment.setArguments(bundle);
-		return pageFragment;
-	}
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 20; i++) {
+            builder.append(content).append(" ");
+        }
+        builder.deleteCharAt(builder.length() - 1);
+        fragment.mContent = builder.toString();
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
+        return fragment;
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+    private String mContent = "???";
 
-		View view = inflater.inflate(R.layout.fragment, container, false);
-		TextView mTitle = (TextView) view.findViewById(R.id.tv_fragment_title);
-		mTitle.setText(getArguments().getString("title"));
-		return view;
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if ((savedInstanceState != null) && savedInstanceState.containsKey(KEY_CONTENT)) {
+            mContent = savedInstanceState.getString(KEY_CONTENT);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        TextView text = new TextView(getActivity());
+        text.setGravity(Gravity.CENTER);
+        text.setText(mContent);
+        text.setTextSize(20 * getResources().getDisplayMetrics().density);
+        text.setPadding(20, 20, 20, 20);
+
+        LinearLayout layout = new LinearLayout(getActivity());
+        layout.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+        layout.setGravity(Gravity.CENTER);
+        layout.addView(text);
+
+        return layout;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(KEY_CONTENT, mContent);
+    }
 }
