3aed23e6be41adadb425ccc9b1b4d77e9745aa9e
Tuan Anh Le <skillerr@gmail.com>
2012-09-29 23:48:29 +0200

    Added links
    but not clickable yet

--- MegaLike/src/com/atlast/MegaLike/StatusesTabFragment.java
+++ MegaLike/src/com/atlast/MegaLike/StatusesTabFragment.java
@@ -6,6 +6,8 @@ import java.util.Vector;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,8 +19,8 @@ import com.atlast.MegaLike.Lib.Extra;
 
 public final class StatusesTabFragment extends Fragment {
 	private Vector<Link> mLinks;
-	private Vector<String> mStatuses = new Vector<String>();
-	
+	private Vector<Spanned> mStatuses = new Vector<Spanned>();
+
 	public static StatusesTabFragment newInstance() {
 		StatusesTabFragment fragment = new StatusesTabFragment();
 		return fragment;
@@ -29,14 +31,19 @@ public final class StatusesTabFragment extends Fragment {
 		super.onAttach(activity);
 		mLinks = Extra.mFacebookData.getLinks(Extra.CURRENT_FRIEND_UID);
 		Collections.sort(mLinks);
-		for(Link link : mLinks)
-			mStatuses.add(link.status);
+		for (Link link : mLinks) {
+			if (link.url != null && link.linkTitle != null) {
+				String linkUrl = link.url.replaceAll("gdata.youtube.com/feeds/api/videos/", "www.youtube.com/watch?v=");
+				mStatuses.add(Html.fromHtml(link.status + "\n<a href=" + linkUrl + ">" + link.linkTitle + "</a>"));
+			} else
+				mStatuses.add(Html.fromHtml(link.status));
+		}
 	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 	}
 
 	@Override
@@ -48,9 +55,8 @@ public final class StatusesTabFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.statusestabfragment, container, false);
 		ListView listView = (ListView) view.findViewById(R.id.statusestabfragment_listview);
-		
-			
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, mStatuses.toArray(new String[mStatuses.size()]));
+
+		ArrayAdapter<Spanned> adapter = new ArrayAdapter<Spanned>(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, mStatuses.toArray(new Spanned[mStatuses.size()]));
 		listView.setAdapter(adapter);
 		return view;
 	}
