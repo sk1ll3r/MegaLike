812bbd2e9b1c77529e2f961db072e0e7faf9807d
Tuan Anh Le <skillerr@gmail.com>
2012-11-08 00:27:46 +0000

    Added AsyncTask for login tasks
    shouldn't crash on newer androids anymore

--- MegaLike/src/com/atlast/MegaLike/Lib/Extra.java
+++ MegaLike/src/com/atlast/MegaLike/Lib/Extra.java
@@ -17,7 +17,7 @@ public class Extra {
 	public static int THEME = com.actionbarsherlock.R.style.Theme_Sherlock_ForceOverflow;
 	public static String CURRENT_USER_UID = null;
 	public static String CURRENT_FRIEND_UID = null;
-	public static String[] PERMISSIONS = new String[] { "user_photos", "friends_photos", "publish_stream", "read_stream", "friends_status", "user_status" };
+	public static String[] PERMISSIONS = new String[] { "user_photos", "friends_photos", "friends_status", "user_status" };
 	public static String APP_ID= new String("367951253282551");
 
 	public static DisplayImageOptions DISPLAY_IMAGE_OPTIONS = new DisplayImageOptions.Builder().showStubImage(R.drawable.stub_image).showImageForEmptyUri(R.drawable.image_for_empty_url).cacheInMemory().cacheOnDisc().build();
